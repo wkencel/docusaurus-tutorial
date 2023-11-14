fda77837cd8e7b1334fe3bd5bab8874e6c8f2960
wkencel <wkencel1@gmail.com>
2023-11-14 17:12:02 -0500

    change the name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Rafay',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -66,9 +66,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Rafay',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Rafay Logo',
           src: 'img/logo.svg',
         },
         items: [
