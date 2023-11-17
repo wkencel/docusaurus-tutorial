c0cec4629f3b24ac4bf60d9eaa80853ec08d03f2
charliehinojosa <107094685+charliehinojosa@users.noreply.github.com>
2023-11-16 16:05:22 -0800

    Update index.js
    text changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,14 +13,14 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
-          {siteConfig.title}
+          Infrastructure Automation for Generative AI
         </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className="hero__subtitle">Automate the Infrastructure that drives your Company’s AI Journey</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Explore Templates 🚀
           </Link>
         </div>
       </div>
